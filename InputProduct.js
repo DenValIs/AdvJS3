ef110ea4255fedf2caca62e262fb731edb6e610e
DenValIs <DenValIs@mail.ru>
2024-01-15 20:55:52 +0300

    HW_3_AdvJS3
    upload hw

---
+++ InputProduct.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const inputProduct = document.querySelector('.input__product');
+const inputRewiew = document.querySelector('.input__rewiew');
+const buttonSubmit = document.querySelector('.button__submit');
+const messageError = document.querySelector('.message__error');
+
+buttonSubmit.addEventListener("click", () => {
+   const product = inputProduct.value;
+   const rewiew = inputRewiew.value;
+   if (product !== "" && rewiew !== "") {
+      let store = JSON.parse(localStorage.getItem(product));
+      if (store === null) {
+         store = []
+      }
+      store.push(rewiew);
+      localStorage.setItem(product, JSON.stringify(store));
+   } else {
+      messageError.textContent = "Введены не все поля ввода"
+   }
+});
